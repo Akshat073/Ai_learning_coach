95aa122b89887d4770c614ada91ca72af0355023
Suryansh Singh <137262891+suryansh1440@users.noreply.github.com>
2025-02-13 21:18:09 +0530

    cors update

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.use(cors({
 app.use(helmet({
   crossOriginResourcePolicy: false,
 }));
-app.use(morgan());
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
